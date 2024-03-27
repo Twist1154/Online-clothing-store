86194be2e2d99a82100bfc67d10babe2f39a0b03
ZEDAYa1 <220097429@mycput.ac.za>
2024-03-27 23:14:25 +0200

    ProductImageFactory not completed due to blob implementation difficulty

--- src/test/java/za/ac/cput/factory/ProductImageFactoryTest.java
+++ src/test/java/za/ac/cput/factory/ProductImageFactoryTest.java
@@ -1,4 +1,33 @@
 package za.ac.cput.factory;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+/*
+ *Product:java
+ *Product: ProductImageFactoryTest Class
+ * Author: Zachariah Matsimella
+ * Date: 26 March 2024
+ */
 public class ProductImageFactoryTest {
+
+    @BeforeEach
+    void setUp(){
+
+    }
+
+    @Test
+    @Disabled
+    @Timeout(200)
+    public void disabled(){
+        System.out.println("This test should be disabled. If not then a timout should occur in 2 seconds.");
+    }
+
+    @Test
+    @Timeout(5)
+    public void timeout() throws InterruptedException{
+        Thread.sleep(2000);
+    }
 }
