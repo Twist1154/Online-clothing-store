8f57fbbfc4a469645b11c392ffee2348d34702f2
Semoshwe <semoshwemapokgole@gmail.com>
2024-05-17 20:03:44 +0200

    semoshwe

--- src/main/java/za.ac.cput/OnlineClothingStoreApp.java
+++ src/main/java/za.ac.cput/OnlineClothingStoreApp.java
@@ -1,8 +1,10 @@
 package za.ac.cput;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@Configuration
 public class OnlineClothingStoreApp {
     public static void main(String[] args){
 
--- src/main/java/za.ac.cput/util/Helper.java
+++ src/main/java/za.ac.cput/util/Helper.java
@@ -7,9 +7,11 @@ package za.ac.cput.util;
  */
 
 import za.ac.cput.domain.Order;
+import za.ac.cput.domain.User;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.regex.Pattern;
 
 public class Helper {
 
@@ -29,4 +31,13 @@ public class Helper {
     public static boolean isNullOrEmpty(List<Order> s) {
         return s == null || s.isEmpty();
     }
+
+    public static boolean isEmailValid(String emailAddress) {
+        String regexemail ="^(?=.{1,64}@)[A-Za-z0-9_-]+(\\\\.[A-Za-z0-9_-]+)*@\" \n" +
+                "        + \"[^-][A-Za-z0-9-]+(\\\\.[A-Za-z0-9-]+)*(\\\\.[A-Za-z]{2,})$";
+        return Pattern.compile(regexemail)
+                .matcher(emailAddress)
+                .matches();
+    }
+
 }
