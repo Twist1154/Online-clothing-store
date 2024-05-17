cd8548e420c2a759105fc355e2e2e8fc4a76a811
ZEDAYa1 <220097429@mycput.ac.za>
2024-05-17 22:01:12 +0200

    ProductImage repository and Product repository updated

--- src/main/java/za.ac.cput/repository/IProductRepository.java
+++ src/main/java/za.ac.cput/repository/IProductRepository.java
@@ -1,9 +1,12 @@
 package za.ac.cput.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import za.ac.cput.domain.Product;
 
 import java.util.List;
 
-public interface IProductRepository extends IRepository<Product, String>{
+@Repository
+public interface IProductRepository extends JpaRepository<Product, String> {
     List<Product> getAll();
 }
