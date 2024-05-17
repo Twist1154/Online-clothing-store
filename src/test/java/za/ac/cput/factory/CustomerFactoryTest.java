3f52803e99ddf22c1faa3ef0aa812752ae521954
Twist1154n <rethabile1154n@gmail.com>
2024-05-18 00:53:58 +0200

    who changed my repo

--- src/test/java/za/ac/cput/factory/CustomerFactoryTest.java
+++ src/test/java/za/ac/cput/factory/CustomerFactoryTest.java
@@ -1,9 +1,8 @@
 package za.ac.cput.factory;
 
-import org.junit.jupiter.api.Order;
+
 import org.junit.jupiter.api.Test;
 import za.ac.cput.domain.Customer;
-import za.ac.cput.repository.ICustomerRepo;
 import za.ac.cput.repository.ICustomerRepo;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -11,10 +10,8 @@ import static org.junit.jupiter.api.Assertions.*;
 class CustomerFactoryTest {
 
     private static Customer customer;
-    private static final ICustomerRepo repository = ICustomerRepo.getRepository();
 
     @Test
-    @Order(1)
     void createCustomer() {
         Customer createdCustomer = CustomerFactory.createCustomer("123", "Premium", "user123");
         assertNotNull(createdCustomer);
@@ -24,32 +21,4 @@ class CustomerFactoryTest {
         customer = createdCustomer;
     }
 
-    @Test
-    @Order(2)
-    void read() {
-        Customer readCustomer = repository.read(customer.getCustomerId());
-        assertNotNull(readCustomer);
-        assertEquals(customer, readCustomer);
-    }
-
-    @Test
-    @Order(3)
-    void update() {
-        Customer updatedCustomer = new Customer.Builder()
-                .copy(customer)
-                .setPrivelages("Basic")
-                .build();
-
-        Customer updated = repository.update(updatedCustomer);
-        assertNotNull(updated);
-        assertEquals("Basic", updated.getPrivelages());
-    }
-
-    @Test
-    @Order(4)
-    void delete() {
-        boolean deleted = repository.delete(customer.getCustomerId());
-        assertTrue(deleted);
-        assertNull(repository.read(customer.getCustomerId()));
-    }
 }
