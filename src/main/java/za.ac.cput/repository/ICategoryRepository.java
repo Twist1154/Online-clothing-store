d02d2983e1ffb9718680aa03f64ec93fcfd89cc2
ZEDAYa1 <220097429@mycput.ac.za>
2024-05-17 23:55:43 +0200

    Fixed Category repository duplicate

--- src/main/java/za.ac.cput/repository/ICategoryRepository.java
+++ src/main/java/za.ac.cput/repository/ICategoryRepository.java
@@ -1,6 +1,6 @@
 /**
  * E-Commerce Web Application for selling clothes
- * ICategoryRepository.java
+ * CategoryRepository.java
  * This class provides the interface for the Category entity
  * Author: Mthandeni Mbobo - 218223579
  * Date: 22 March 2024
@@ -8,10 +8,12 @@
 
 package za.ac.cput.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import za.ac.cput.domain.Category;
 
-import java.util.List;
-
-public interface ICategoryRepository extends IRepository<Category, String>{
-    List<Category> getAll();
+@Repository
+public interface ICategoryRepository extends JpaRepository<Category, String> {
+    Category findCategoryByCategoryName(String categoryName);
+    boolean deleteCategoryByCategoryName(String categoryName);
 }
