2c3e2ee0e1e043f9aacb7d50d16def8e0316beb2
Luke <217061567@mycput.ac.za>
2024-05-17 21:45:14 +0200

    Repository Update

---
+++ src/main/java/za.ac.cput/repository/AdminRepository.java
@@ -0,0 +1,16 @@
+package za.ac.cput.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import za.ac.cput.domain.Admin;
+import java.util.List;
+
+/*
+ *IAdminRepository:java
+ *Admin: Repository Class
+ * Author: Luke Duffell
+ * Student no: 217061567
+ * Date: 17 May 2024
+ */
+@Repository
+public interface AdminRepository extends JpaRepository<Admin, String>{
+}
