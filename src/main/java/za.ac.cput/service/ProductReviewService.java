5e9e46c38a9c3682aaae3da9dbbdf711bc85133b
mbobomthandeni <218223579@mycput.ac.za>
2024-05-19 18:33:12 +0200

    Added service classes. CategoryService & ProductReviewService

---
+++ src/main/java/za.ac.cput/service/ProductReviewService.java
@@ -0,0 +1,49 @@
+/*
+* ProductReviewService.java
+* Service for the ProductReview
+* Author: Mthandeni Mbobo (218223579)
+* Date: 18 May 2024
+* */
+
+package za.ac.cput.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import za.ac.cput.domain.ProductReview;
+import za.ac.cput.repository.ProductReviewRepository;
+
+import java.util.List;
+
+@Service
+public class ProductReviewService implements IProductReviewService{
+
+    private ProductReviewRepository repository;
+
+    @Autowired
+    ProductReviewService(ProductReviewRepository repository){
+        this.repository = repository;
+    }
+
+    @Override
+    public ProductReview create(ProductReview productReview){
+        return this.repository.save(productReview);
+    }
+
+    @Override
+    public ProductReview read(String productReviewID){
+        return this.repository.findById(productReviewID).orElse(null);
+    }
+
+    @Override
+    public ProductReview update(ProductReview productReview){
+        if (this.repository.existsById(productReview.getProductReviewID()))
+            return this.repository.save(productReview);
+        return null;
+    }
+
+    @Override
+    public List<ProductReview> getAll(){
+        return this.repository.findAll();
+    }
+
+}
