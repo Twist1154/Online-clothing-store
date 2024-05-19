28e1ce51843c1de9e1121e32f65946044134e502
mbobomthandeni <218223579@mycput.ac.za>
2024-05-19 18:31:17 +0200

    Added services interfaces. ICategoryService & IProductReviewService

--- src/main/java/za.ac.cput/service/ICategoryService.java
+++ src/main/java/za.ac.cput/service/ICategoryService.java
@@ -3,6 +3,7 @@
 * Service Interface for the Category
 * Author: Mthandeni Mbobo (218223579)
 * Date: 18 May 2024
+* ---
 * */
 
 package za.ac.cput.service;
--- src/main/java/za.ac.cput/service/IProductReviewService.java
+++ src/main/java/za.ac.cput/service/IProductReviewService.java
@@ -3,6 +3,7 @@
 * Service Interface for the ProductReview
  * Author: Mthandeni Mbobo (218223579)
  * Date: 18 May 2024
+ * --
 * */
 
 package za.ac.cput.service;
