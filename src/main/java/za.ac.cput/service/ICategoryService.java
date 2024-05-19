9e8ccd4f84c3ff5dcc5473f80a37fc7bbc179188
mbobomthandeni <218223579@mycput.ac.za>
2024-05-19 18:29:26 +0200

    Added services interfaces. ICategoryService & IProductReviewService

---
+++ src/main/java/za.ac.cput/service/ICategoryService.java
@@ -0,0 +1,16 @@
+/*
+* ICategoryService.java
+* Service Interface for the Category
+* Author: Mthandeni Mbobo (218223579)
+* Date: 18 May 2024
+* */
+
+package za.ac.cput.service;
+
+import za.ac.cput.domain.Category;
+
+import java.util.List;
+
+public interface ICategoryService extends IService<Category, String>{
+    List<Category> getAll();
+}
---
+++ src/main/java/za.ac.cput/service/IProductReviewService.java
@@ -0,0 +1,16 @@
+/*
+* IProductReviewService.java
+* Service Interface for the ProductReview
+ * Author: Mthandeni Mbobo (218223579)
+ * Date: 18 May 2024
+* */
+
+package za.ac.cput.service;
+
+import za.ac.cput.domain.ProductReview;
+
+import java.util.List;
+
+public interface IProductReviewService extends IService<ProductReview, String>{
+    List<ProductReview> getAll();
+}
