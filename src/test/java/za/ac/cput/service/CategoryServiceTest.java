5ad6080f35b7e9b8e831e0042f23dd29085a2908
mbobomthandeni <218223579@mycput.ac.za>
2024-05-19 19:34:14 +0200

    Added service tests. CategoryServiceTest & ProductReviewServiceTest

---
+++ src/test/java/za/ac/cput/service/CategoryServiceTest.java
@@ -0,0 +1,77 @@
+/*
+* CategoryServiceTest.java
+* Test for the CategoryService
+* Author: Mthandeni Mbobo (218223579)
+* Date: 18 May 2024
+* */
+
+package za.ac.cput.service;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import za.ac.cput.domain.Category;
+import za.ac.cput.factory.CategoryFactory;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class CategoryServiceTest {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    private static Category category;
+    private static Category category2;
+
+    @Test
+    @Order(1)
+    void setup(){
+        category = CategoryFactory.buildCategory("Men"); //id auto generated
+        assertNotNull(category);
+        System.out.println("Category: " + category);
+
+        category2 = CategoryFactory.buildCategory("3","Kids"); //id inserted manually
+        assertNotNull(category2);
+        System.out.println("Category2: " + category2);
+    }
+
+    @Test
+    @Order(2)
+    void create() {
+        Category created = categoryService.create(category);
+        assertEquals(category.getCategoryID(), created.getCategoryID());
+        System.out.println("Created: " + created);
+
+        Category created2 = categoryService.create(category2);
+        assertEquals(category2.getCategoryID(), created2.getCategoryID());
+        System.out.println("Created2: " + created2);
+    }
+
+    @Test
+    @Order(3)
+    void read() {
+        Category read = categoryService.read(category.getCategoryID());
+        assertNotNull(read);
+        System.out.println("Read category: " + read);
+    }
+
+    @Test
+    @Order(4)
+    void update() {
+        Category newCategory = new Category.Builder().copy(category).setCategoryName("Mens").build();
+        Category updated = categoryService.update(newCategory);
+        assertEquals(newCategory.getCategoryName(), updated.getCategoryName());
+        System.out.println("Updated category: " + updated);
+    }
+
+    @Test
+    @Order(5)
+    void getAll() {
+        System.out.println("All categories" + categoryService.getAll());
+    }
+}
---
+++ src/test/java/za/ac/cput/service/ProductReviewServiceTest.java
@@ -0,0 +1,79 @@
+package za.ac.cput.service;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import za.ac.cput.domain.ProductReview;
+import za.ac.cput.factory.ProductReviewFactory;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class ProductReviewServiceTest {
+
+    @Autowired
+    private ProductReviewService productReviewService;
+
+    private static ProductReview productReview;
+    private static ProductReview productReview2;
+    private static ProductReview productReview3;
+
+    @Test
+    @Order(1)
+    void setup(){
+        productReview = ProductReviewFactory.buildProductReview("1", "1", "I am really impressed with this jean", 5); //id auto generated
+        assertNotNull(productReview);
+        System.out.println("ProductReview: " + productReview);
+
+        productReview2 = ProductReviewFactory.buildProductReview("1", "1", "1", "Quality on this jean is really not up to standard", 2); //id inserted manually
+        assertNotNull(productReview2);
+        System.out.println("ProductReview2: " + productReview2);
+
+        productReview3 = ProductReviewFactory.buildProductReview("2", "2", "2", "I am really impressed with this t-shirt", 5); //id inserted manually
+        assertNotNull(productReview3);
+        System.out.println("ProductReview3: " + productReview3);
+    }
+
+    @Test
+    @Order(2)
+    void create() {
+        ProductReview created = productReviewService.create(productReview);
+        assertEquals(productReview.getProductReviewID(), created.getProductReviewID());
+        System.out.println("Created: " + created);
+
+        ProductReview created2 = productReviewService.create(productReview2);
+        assertEquals(productReview2.getProductReviewID(), created2.getProductReviewID());
+        System.out.println("Created2: " + created2);
+
+        ProductReview created3 = productReviewService.create(productReview3);
+        assertEquals(productReview3.getProductReviewID(), created3.getProductReviewID());
+        System.out.println("Created3: " + created3);
+    }
+
+    @Test
+    @Order(3)
+    void read() {
+        ProductReview read = productReviewService.read(productReview.getProductReviewID());
+        assertNotNull(read);
+        System.out.println("Read productReview: " + read);
+    }
+
+    @Test
+    @Order(4)
+    void update() {
+        ProductReview newProductReview = new ProductReview.Builder().copy(productReview).setReview("I made a mistake, this thing is awful").setRating(1).build();
+        ProductReview updated = productReviewService.update(newProductReview);
+        assertEquals(newProductReview.getRating(), updated.getRating());
+        System.out.println("Updated productReview: " + updated);
+    }
+
+    @Test
+    @Order(5)
+    void getAll() {
+        System.out.println("All productReviews" + productReviewService.getAll());
+    }
+}
