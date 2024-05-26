7a54e01fdcf08f885e24d880ba7de8e6f4234af9
ZEDAYa1 <220097429@mycput.ac.za>
2024-05-26 18:34:57 +0200

    Created Product and ProductImage controller test classes.

---
+++ src/test/java/za/ac/cput/controller/ProductImageControllerTest.java
@@ -0,0 +1,24 @@
+package za.ac.cput.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductImageControllerTest {
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void read() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void getAll() {
+    }
+}
